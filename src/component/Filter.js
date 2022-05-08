325098a0c3c5cfe623622f1d9b6326f074c4868c
Istiak98 <86631588+Istiak98@users.noreply.github.com>
2022-05-09 03:06:22 +0600

    filter componet refactor

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -1,32 +1,11 @@
 import React from "react";
+import { launchDate, launchStatus, launchUpcoming } from "../utlis/filterData";
+import DropDownButton from "./DropDownButton";
 const Filter = ({ sortByDate, sortByStatus, sortByUpcoming }) => {
-  const launchDate = [
-    {
-      name: "Last Week",
-      day: 7,
-    },
-    {
-      name: "Last Month",
-      day: 30,
-    },
-    {
-      name: "Last Year",
-      day: 365,
-    },
-  ];
-
   return (
     <div className="d-flex align-items-center justify-content-center gap-3">
       <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton2"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Launch Date
-        </button>
+        <DropDownButton name="Launch Date" />
         <ul
           class="dropdown-menu dropdown-menu-dark"
           aria-labelledby="dropdownMenuButton2"
@@ -38,71 +17,63 @@ const Filter = ({ sortByDate, sortByStatus, sortByUpcoming }) => {
                   {item.name}
                 </p>
               </li>
-              {index !== launchDate.length - 1 && 
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              }
+              {index !== launchDate.length - 1 && (
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+              )}
             </>
           ))}
         </ul>
       </div>
       <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton2"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Launch Status
-        </button>
+        <DropDownButton name="Launch Status" />
         <ul
           class="dropdown-menu dropdown-menu-dark"
           aria-labelledby="dropdownMenuButton2"
         >
-          <li>
-            <p class="dropdown-item" onClick={() => sortByStatus(true)}>
-              Success
-            </p>
-          </li>
-          <li>
-            <hr class="dropdown-divider" />
-          </li>
-          <li>
-            <p class="dropdown-item" onClick={() => sortByStatus(false)}>
-              Failure
-            </p>
-          </li>
+          {launchStatus.map((item, index) => (
+            <>
+              <li>
+                <p
+                  class="dropdown-item"
+                  onClick={() => sortByStatus(item.status)}
+                >
+                  {item.name}
+                </p>
+              </li>
+              {index !== launchStatus.length - 1 && (
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+              )}
+            </>
+          ))}
         </ul>
       </div>
       <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton2"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Upcoming
-        </button>
+        <DropDownButton name="Upcoming" />
         <ul
           class="dropdown-menu dropdown-menu-dark"
           aria-labelledby="dropdownMenuButton2"
         >
-          <li>
-            <p class="dropdown-item" onClick={() => sortByUpcoming(true)}>
-              True
-            </p>
-          </li>
-          <li>
-            <hr class="dropdown-divider" />
-          </li>
-          <li>
-            <p class="dropdown-item" onClick={() => sortByUpcoming(false)}>
-              False
-            </p>
-          </li>
+          {launchUpcoming.map((item, index) => (
+            <>
+              <li>
+                <p
+                  class="dropdown-item"
+                  onClick={() => sortByUpcoming(item.status)}
+                >
+                  {item.name}
+                </p>
+              </li>
+              {index !== launchUpcoming.length - 1 && (
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+              )}
+            </>
+          ))}
         </ul>
       </div>
     </div>
